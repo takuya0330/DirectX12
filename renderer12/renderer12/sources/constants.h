cec7e9e6978eb6be3b673c156564e7f3f73bc804
takuya <takuya0330_a@yahoo.co.jp>
2020-07-08 17:18:30 +0900

    整理

--- renderer12/renderer12/sources/constants.h
+++ renderer12/renderer12/sources/constants.h
@@ -5,5 +5,4 @@ namespace constants
 {
 	constexpr int2 kResolution = int2(1600, 900);
 	constexpr float kAspectRatio = static_cast<float>(kResolution.x) / static_cast<float>(kResolution.y);
-	constexpr double kPI = 3.14159265358979;
 }
